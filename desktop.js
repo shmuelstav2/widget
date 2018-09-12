8c0097883fa50dfa2ca28b4e9a3acea25de208ad
smulic <shmhad123@gmail.com>
2018-09-12 20:23:06 +0300

    Update links to the server

--- desktop.js
+++ desktop.js
@@ -87,7 +87,7 @@ function loadTxtPlugin() {
             }
         });
         var dic = {};
-        var mainUrl = 'http://localhost:80/getAmazonUrl/';
+        var mainUrl = 'http://txtrider.co/getAmazonUrl/';
         let payLoad = {
             url: window.location.href
         };
