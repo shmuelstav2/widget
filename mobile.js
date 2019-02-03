5c522f7a9742b86e3579854adccacbd0a95fc545
shmuelstav2 <35642414+shmuelstav2@users.noreply.github.com>
2019-02-03 13:45:28 +0200

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -365,8 +365,8 @@ function loadTxtPlugin() {
     var dic = {};
     var lowcaseDic = {};
     
-    // var mainUrl = 'https://server.txtrider.com';
-    var mainUrl = 'https://txtrider.co';
+     var mainUrl = 'https://server.txtrider.com';
+    //var mainUrl = 'https://txtrider.co';
     $('head').append('<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">');
     if (debug) {
         success(testMock)
