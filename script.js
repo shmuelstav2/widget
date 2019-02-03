54543e71c96a279c8f157843adf33458724c98c7
shmuelstav2 <35642414+shmuelstav2@users.noreply.github.com>
2019-02-03 13:43:02 +0200

    change to prod

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-    var base = 'http://txtrider.co/txtRider-publisher-panel/admin_1_angularjs/widget/';
+    var base = 'https://server.txtrider.com/txtRider-publisher-panel/admin_1_angularjs/widget/';
     var desktopScriptUrl = base + 'desktop.js'
     var mobileScriptUrl= base + 'mobile.js';
     var isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Opera Mobile|Kindle|Windows Phone|PSP|AvantGo|Atomic Web Browser|Blazer|Chrome Mobile|Dolphin|Dolfin|Doris|GO Browser|Jasmine|MicroB|Mobile Firefox|Mobile Safari|Mobile Silk|Motorola Internet Browser|NetFront|NineSky|Nokia Web Browser|Obigo|Openwave Mobile Browser|Palm Pre web browser|Polaris|PS Vita browser|Puffin|QQbrowser|SEMC Browser|Skyfire|Tear|TeaShark|UC Browser|uZard Web|wOSBrowser|Yandex.Browser mobile/i.test(navigator.userAgent))
